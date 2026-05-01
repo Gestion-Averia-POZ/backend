2b673cd839ee8b386ccda316b458ae71e06f5c3e
Luis Cabrera <cabrerasluis67@gmail.com>
2026-05-01 14:27:38 -0400

    fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import { reportsRoutes } from '../modules/reports';
 import { categoriesRoutes } from '../modules/categories';
 import { companiesRoutes } from '../modules/companies';
 import { failureTypeRoutes } from '../modules/failure-types';
+import { notificationsRoutes } from '../modules/notifications';
 import healthRoutes from './health.routes';
 
 const router = Router();
@@ -15,6 +16,7 @@ router.use('/reports', reportsRoutes);
 router.use('/categories', categoriesRoutes);
 router.use('/companies', companiesRoutes);
 router.use('/failure-types', failureTypeRoutes);
+router.use('/notifications', notificationsRoutes);
 router.use('/health', healthRoutes);
 
 export default router;
